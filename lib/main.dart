35d04161bfa880fac4e3e4104ea1c5c426021415
Ali Sheykhi <alishi.dev@gmail.com>
2023-03-25 11:36:28 +0330

    send packet func

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+import 'dart:io';
 import 'package:flutter/material.dart';
+//import 'package:network_info_plus/network_info_plus.dart';
 
 void main() {
   runApp(const MyApp());
@@ -31,85 +34,130 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
+
+
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  var messageController = TextEditingController();
+  var ipController = TextEditingController();
+  var portController = TextEditingController();
+ // final String _ipAddress = '';
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+
+  @override
+  void initState() {
+    super.initState();
+    //_getIp();
   }
+
+  Future<void> sendUdpPacket(String ip, int port, String message) async {
+    try {
+      // Create a UDP socket
+      RawDatagramSocket udpSocket =
+      await RawDatagramSocket.bind(InternetAddress.anyIPv4, 0);
+
+      // Send the message to the remote device
+      List<int> data = utf8.encode(message);
+      udpSocket.send(
+        data,
+        InternetAddress(ip),
+        port,
+      );
+
+      // Close the socket
+      udpSocket.close();
+    } catch (e) {
+      debugPrint(e.toString());
+    }
+  }
+  // Future<void> _getIp() async {
+  //   String? ipAddress;
+  //
+  //   try {
+  //     ipAddress = await NetworkInfo().getWifiIP();
+  //   } catch (e) {
+  //     print(e.toString());
+  //     ipAddress = 'Failed to get IP address.';
+  //   }
+  //
+  //   setState(() {
+  //     _ipAddress = ipAddress;
+  //   });
+  // }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+        child: Container(
+          padding: const EdgeInsets.all(8.0),
+          margin: const EdgeInsets.all(10.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisSize: MainAxisSize.max,
+            children: <Widget>[
+              // Text(
+              //   'IP Address $_ipAddress',
+              // ),
+              const Text(
+                'Enter Destination IP Address',
+              ),
+              TextField(
+                controller: ipController,
+                keyboardType: TextInputType.emailAddress,
+                decoration: const InputDecoration(
+                  hintText: "192.168.1.100 for iphone",
+                ),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              const Text(
+                'Enter Destination Port Number',
+              ),
+              TextField(
+                controller: portController,
+                keyboardType: TextInputType.number,
+                decoration: const InputDecoration(
+                  hintText: "1234",
+                ),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              const Text(
+                'Enter Your Message ',
+              ),
+              TextField(
+                controller: messageController,
+                decoration: const InputDecoration(hintText: "Hello World!"),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  sendUdpPacket(
+                    ipController.text,
+                    int.parse(portController.text),
+                    messageController.text,
+                  );
+                },
+                child: const Text('Send Message'),
+              )
+            ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
